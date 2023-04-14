4640e95337fce5943f668f65e0486769e2c4e2f3
Anton-Wall <Anton.wl.wall@gmail.com>
2023-04-14 14:29:55 +0200

    new

--- code.py
+++ code.py
@@ -10,49 +10,24 @@ Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
 Building instructions can be found at:
 https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#building-core
 """
-
+#import modules
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.parameters import Port, Stop, Direction
 from pybricks.tools import wait
 
-# Initialize the EV3 Brick
+#define motors and sensors
 ev3 = EV3Brick()
 
-# Configure the gripper motor on Port A with default settings.
-grab = Motor(Port.A)
+grab = Motor(Port.A) #Gripp claw
+arm = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])#elbow motor
+rotate = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])#rotation motor
+arm.control.limits(speed=60, acceleration=120)#Limits in extention of the arm
+rotate.control.limits(speed=60, acceleration=120)#rotation limits
+base_switch = TouchSensor(Port.S1) #base tutch sensor
+arm_sensor = ColorSensor(Port.S3) #collor sensor
 
-# Configure the elbow motor. It has an 8-teeth and a 40-teeth gear
-# connected to it. We would like positive speed values to make the
-# arm go upward. This corresponds to counterclockwise rotation
-# of the motor.
-arm = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
-
-# Configure the motor that rotates the base. It has a 12-teeth and a
-# 36-teeth gear connected to it. We would like positive speed values
-# to make the arm go away from the Touch Sensor. This corresponds
-# to counterclockwise rotation of the motor.
-rotate = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
-
-# Limit the elbow and base accelerations. This results in
-# very smooth motion. Like an industrial robot.
-arm.control.limits(speed=60, acceleration=120)
-rotate.control.limits(speed=60, acceleration=120)
-
-# Set up the Touch Sensor. It acts as an end-switch in the base
-# of the robot arm. It defines the starting point of the base.
-base_switch = TouchSensor(Port.S1)
-
-# Set up the Color Sensor. This sensor detects when the elbow
-# is in the starting position. This is when the sensor sees the
-# white beam up close.
-arm_sensor = ColorSensor(Port.S3)
-
-# Initialize the elbow. First make it go down for one second.
-# Then make it go upwards slowly (15 degrees per second) until
-# the Color Sensor detects the white beam. Then reset the motor
-# angle to make this the zero point. Finally, hold the motor
-# in place so it does not move.
+#reset the arm
 arm.run_time(-30, 1000)
 arm.run(15)
 while arm_sensor.reflection() < 32:
@@ -60,81 +35,65 @@ while arm_sensor.reflection() < 32:
 arm.reset_angle(0)
 arm.hold()
 
-# Initialize the base. First rotate it until the Touch Sensor
-# in the base is pressed. Reset the motor angle to make this
-# the zero point. Then hold the motor in place so it does not move.
+#reset the base of the arm
 rotate.run(-60)
 while not base_switch.pressed():
     wait(10)
 rotate.reset_angle(0)
 rotate.hold()
 
-# Initialize the gripper. First rotate the motor until it stalls.
-# Stalling means that it cannot move any further. This position
-# corresponds to the closed position. Then rotate the motor
-# by 90 degrees such that the gripper is open.
+#reset the claw
 grab.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
 grab.reset_angle(0)
 grab.run_target(200, -90)
 
 
 def robot_pick(position):
-    # This function makes the robot base rotate to the indicated
-    # position. There it lowers the elbow, closes the gripper, and
-    # raises the elbow to pick up the object.
+    #pick upp the brick
 
-    # Rotate to the pick-up position.
+    # move to pickup position
     rotate.run_target(60, position)
-    # Lower the arm.
+    # Lower the arm
     arm.run_target(60, -40)
-    # Close the gripper to grab the wheel stack.
+    # grab the block
     grab.run_until_stalled(200, then=Stop.HOLD, duty_limit=50)
-    # Raise the arm to lift the wheel stack.
+    # Raise the arm
     arm.run_target(60, 0)
 
 
 def robot_release(position):
-    # This function makes the robot base rotate to the indicated
-    # position. There it lowers the elbow, opens the gripper to
-    # release the object. Then it raises its arm again.
+    #the robot drops the brick on the correkt position
 
-    # Rotate to the drop-off position.
+    # Rotate to the drop-off position
     rotate.run_target(60, position)
-    # Lower the arm to put the wheel stack on the ground.
+    # Lower the arm
     arm.run_target(60, -40)
-    # Open the gripper to release the wheel stack.
+    # Open the gripper to relise the brick
     grab.run_target(200, -90)
-    # Raise the arm.
+    # Raise the arm
     arm.run_target(60, 0)
 
 
-# Play three beeps to indicate that the initialization is complete.
+#indikation och reset complete
 for i in range(3):
     ev3.speaker.beep()
     wait(100)
 
-# Define the three destinations for picking up and moving the wheel stacks.
+#Define rotation possitions
 LEFT = 160
 MIDDLE = 100
 RIGHT = 40
 
-# This is the main part of the program. It is a loop that repeats endlessly.
-#
-# First, the robot moves the object on the left towards the middle.
-# Second, the robot moves the object on the right towards the left.
-# Finally, the robot moves the object that is now in the middle, to the right.
-#
-# Now we have a wheel stack on the left and on the right as before, but they
-# have switched places. Then the loop repeats to do this over and over.
+#main loop
 while True:
-    # Move a wheel stack from the left to the middle.
+    # Move a brick from the left to the middle
     robot_pick(LEFT)
     robot_release(MIDDLE)
 
-    # Move a wheel stack from the right to the left.
+    # Move a brick from the right to the left
     robot_pick(RIGHT)
     robot_release(LEFT)
 
-    # Move a wheel stack from the middle to the right.
+    # Move a brick from the middle to the right
     robot_pick(MIDDLE)
     robot_release(RIGHT)
